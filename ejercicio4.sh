a8c1985587f63e224baf59aaf7f3628224f71a02
Ángela Pérez Pérez <angelaperezperezfp@gmail.com>
2016-11-16 20:55:42 +0100

    Eliminar las copias.

--- ejercicio4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! - "$1" ]
-then
-	echo "No existe el usuario"
-	exit 1
-fi
-
-deluser --remove-home $1
--- ejercicio5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "$1" ]
-then
-	echo "No existe el directorio"
-fi
-
-chmod -R u+rwx,g+rwx,o+rwx $1
